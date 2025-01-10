37fd96e8ff082a3bcdbc52107ddea970a95cff08
Andre Zicatti <andrezicatti@gmail.com>
2025-01-10 18:29:47 -0300

    :sparkles: feat: Event Repository, Service and Controller & Update Event

--- src/main/java/com/devsuperior/demo/controllers/EventController.java
+++ src/main/java/com/devsuperior/demo/controllers/EventController.java
@@ -1,4 +1,27 @@
 package com.devsuperior.demo.controllers;
 
+import com.devsuperior.demo.dto.EventDTO;
+import com.devsuperior.demo.services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@Controller
+@RequestMapping(value = "/events")
 public class EventController {
+
+    @Autowired
+    private EventService service;
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<EventDTO> update(@PathVariable Long id, @RequestBody EventDTO dto) {
+        return ResponseEntity.ok().body(service.update(id, dto));
+    }
 }
